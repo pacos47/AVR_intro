e1058e0e3ffbf71bbbf8c46784e04b13d05976db
pacos47 <pacos47@gmail.com>
2022-08-31 01:19:44 +0300

    week1/W1_Vars1.p

---
+++ WEEK_1/W1_Vars1.py
@@ -0,0 +1,17 @@
+x = 3
+name = "Κώστας"
+pi = 3.14
+print(type(x))
+t=type(x)
+tt=type(t)
+print("t is :",t)
+#print(t[0])  error: not subscriptable
+print(tt)
+print(x)
+print(type(22))
+print("7")
+print("type of pi is",type(pi))
+print("type of name is:", type(name))
+# y=pi*100
+print(1.2E2)
+# but piE2 error enw to pi einai orismeno
